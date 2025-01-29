c60c389cf57ae2431cedaf3b09b25d71bcf198c3
PedroVieiraC <pedrovieiratrab@gmail.com>
2025-01-29 12:03:36 -0300

    mudanca no admin admin, tentando fazer a ideia dos botoes em cima funcionar

--- Front-end/js/admin.js
+++ Front-end/js/admin.js
@@ -9,3 +9,32 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
   
+  function mudarEstado(acao) {
+    const titulo = document.getElementById("tituloPrincipal");
+    const descricao = document.getElementById("descricaoPrincipal");
+    const botaoAcao = document.getElementById("btnAcao");
+    
+    switch (acao) {
+        case 'adicionar':
+            titulo.innerText = "Adicionar Equipamento";
+            descricao.innerText = "Preencha os dados do equipamento";
+            botaoAcao.innerText = "Salvar";
+            botaoAcao.classList.remove("btn-danger");
+            botaoAcao.classList.add("btn-primary");
+            break;
+        case 'atualizar':
+            titulo.innerText = "Atualizar Equipamento";
+            descricao.innerText = "Modifique os dados do equipamento";
+            botaoAcao.innerText = "Atualizar";
+            botaoAcao.classList.remove("btn-danger");
+            botaoAcao.classList.add("btn-warning");
+            break;
+        case 'deletar':
+            titulo.innerText = "Deletar Equipamento";
+            descricao.innerText = "Informe o ID do equipamento a ser deletado";
+            botaoAcao.innerText = "Deletar";
+            botaoAcao.classList.remove("btn-primary", "btn-warning");
+            botaoAcao.classList.add("btn-danger");
+            break;
+    }
+  };
